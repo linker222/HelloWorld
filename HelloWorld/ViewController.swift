495bda4c2cf05e1a4fcb134ae3bea1a4c1caf129
shing <shing222@gmail.com>
2017-11-23 22:06:33 +0800

    add my name

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func showMessage() {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello Worldg 施宏政", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
